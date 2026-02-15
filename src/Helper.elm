37437ac8c274960e5722e07ada694b7ea70849cc
Jorge Rivera <up230609@alumnos.upa.edu.mx>
2026-02-15 15:18:31 -0600

    Add Homework Code

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,5 +1,22 @@
 module Helper exposing (..)
 
+import Html exposing (Html, div, h1, li, text, ul)
+
+
+add2 : Int -> Int -> Int
+add2 int1 int2 =
+    int1 + int2
+
+
+add3 : Float -> Float -> Float -> Float
+add3 float1 float2 float3 =
+    float1 + float2 + float3
+
+
+calc : Int -> Int -> (Int -> Int -> Int) -> Int
+calc int1 int2 operator =
+    operator int1 int2
+
 
 languages : List { name : String, releaseYear : Int, currentVersion : String }
 languages =
@@ -62,3 +79,44 @@ videogames =
 getVideogameGenres : List Videogame -> List (List String)
 getVideogameGenres list =
     List.map (\game -> game.genres) list
+
+
+type alias Computer =
+    { ram : String
+    , model : String
+    , brand : String
+    , screenSize : String
+    }
+
+
+myLaptop : Computer
+myLaptop =
+    { ram = "24GB DDR5"
+    , model = "IdeaPad Slim 3"
+    , brand = "LENOVO"
+    , screenSize = "15.6in"
+    }
+
+
+main : Html msg
+main =
+    div
+        []
+        [ h1 [] [ text "My laptop" ]
+        , div []
+            [ ul []
+                [ li []
+                    [ text ("Ram: " ++ .ram myLaptop)
+                    ]
+                , li []
+                    [ text ("Modelo: " ++ .model myLaptop)
+                    ]
+                , li []
+                    [ text ("Marca: " ++ .brand myLaptop)
+                    ]
+                , li []
+                    [ text ("Pulgadas: " ++ .screenSize myLaptop)
+                    ]
+                ]
+            ]
+        ]
